1cf2198cde2c07dd548ba6ff594820f212a2fb69
andrewsilva9 <asilvaa9@gmail.com>
2017-09-14 10:55:31 -0400

    visualization for cpu words

--- visualize_words.py
+++ visualize_words.py
@@ -1,31 +1,40 @@
 import numpy as np
-import pylab as Plot
+import matplotlib.pyplot as plt
 import pickle
 import tsne as ts
 
 
-word_to_embed = pickle.load(open('word_to_embeds.pkl', 'rb'))
-embed_to_word = pickle.load(open('embeds_to_word.pkl', 'rb'))
+word_to_embed = pickle.load(open('cpustuff/cpuwords.pkl', 'rb'))
+embed_to_word = pickle.load(open('cpustuff/wordscpu.pkl', 'rb'))
+for key in word_to_embed.keys():
+    if 'http' in key or '\\x' in key:
+        del word_to_embed[key]
+
 target_words = word_to_embed.keys()[:2000]
 
-rows = [word_to_embed[word].cpu().data.numpy() for word in target_words if word in word_to_embed]
+rows = [word_to_embed[word].cpu().data.numpy()[0] for word in target_words if word in word_to_embed]
 
 target_matrix = np.array(rows)
 reduced_matrix = ts.tsne(target_matrix, 2)
 
-Plot.figure(figsize=(200, 200), dpi=100)
+plt.figure(figsize=(200, 200), dpi=100)
 
-max_x = Math.amax(reduced_matrix, axis=0)[0]
-max_y = Math.amax(reduced_matrix, axis=0)[1]
-Plot.xlim((-max_x, max_x))
-Plot.ylim((-max_y, max_y))
+max_x = np.amax(reduced_matrix, axis=0)[0]
+max_y = np.amax(reduced_matrix, axis=0)[1]
+plt.xlim((-max_x, max_x))
+plt.ylim((-max_y, max_y))
 
-Plot.scatter(reduced_matrix[:, 0], reduced_matrix[:, 1], 20);
+plt.scatter(reduced_matrix[:, 0], reduced_matrix[:, 1], 20)
 
 for row_id in range(0, len(rows)):
-    target_word = embed_to_word[rows[row_id]]
+    target_word = target_words[row_id]
     x = reduced_matrix[row_id, 0]
     y = reduced_matrix[row_id, 1]
-    Plot.annotate(target_word, (x, y))
+    try:
+        plt.annotate(target_word, (x, y))
+    except:
+        print 'emoji maybe'
+        continue
 
-Plot.savefig("my_first_2000.png")
+plt.show()
+plt.savefig("my_first_2000.png")
