e13b61f86bec9303e4b4523d298c9c5342d3fe45
Andrew Silva <asilvaa9@gmail.com>
2017-09-14 15:49:59 -0400

    seq punc strip

--- create_seq_pairs.py
+++ create_seq_pairs.py
@@ -1,7 +1,7 @@
 import os
 import random
 import pickle
-
+import string
 # TODO: currently not preserving conversation context, only message / response. context would help
 
 master_dir = os.path.join(os.getcwd(), 'masters')
@@ -32,7 +32,7 @@ for filename in os.listdir(master_dir):
     index = 0
 
     while index < len(master_txt) - 1:
-        sequence = master_txt[index].split(' ')
+        sequence = master_txt[index].split().translate
         speaker = sequence[0]
 
         # If it's a continuation of a person
@@ -52,6 +52,15 @@ for filename in os.listdir(master_dir):
 
         # Remove speaker tag from sequence
         sequence = sequence[1:]
+
+        # Remove punctuation from sequence
+        for index in range(len(sequence)):
+            sequence[index] = sequence[index].translate(None, string.punctuation)
+            # Remove empty spaces where punctuation used to be
+            if sequence[index] == '':
+                sequence.remove('')
+                index -= 1
+
         if last_speaker is None:
             last_speaker = speaker
         # Friend still speaker:
